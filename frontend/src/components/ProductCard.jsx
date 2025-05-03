5c24ec8ce24d5f1b9d258567a3f6570a0bc719c1
shiv <kumarshiv417@gmail.com>
2025-05-03 22:36:52 +0530

    handle delete and update functionality

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,9 +1,36 @@
-import { Box, Heading, HStack, IconButton, Image, Text } from '@chakra-ui/react'
+import { Box, Button, Heading, HStack, IconButton, Image, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Text, useColorModeValue, useDisclosure, useToast, VStack } from '@chakra-ui/react'
 import { EditIcon, DeleteIcon } from '@chakra-ui/icons'
-import React from 'react'
+import React, { useState } from 'react'
+import { useProductStore } from '../store/product'
 
 const ProductCard = (product) => {
-  console.log("product", product)
+  console.log(product,'ppppppppppppppp')
+  const textColor = useColorModeValue("gray.600","gray.200");
+  const bg = useColorModeValue("white","gray.800");
+  const { isOpen, onOpen, onClose } = useDisclosure()
+ const {deleteProduct} = useProductStore();
+ const toast = useToast();
+ const [updatedProduct, setUpdatedProduct] = useState(product);
+  const handleDeleteProduct = async (pid) => {
+const {success,message} = await deleteProduct(pid);
+if(!success){
+  toast({
+    title:'Error',
+    description:message,
+    status:'error',
+    duration:3000,
+    isClosable:true,
+  })
+} else {
+  toast({
+    title:'Success',
+    description:message,
+    status:'success',
+    duration:3000,
+    isClosable:true
+  })
+}
+  }
   return (
     <Box
       shadow='lg'
@@ -11,18 +38,40 @@ const ProductCard = (product) => {
       overflow='hidden'
       transition='all 0.3sec'
       _hover={{ transform: "translateY(-5px)", shadow: "xl" }}
+      bg={bg}
     >
       <Image src={product.product.image} alt={product.product.name} h={48} w={'full'} objectFit={'cover'} />
       <Box
         p={4}
       >
         <Heading as={'h3'} size={'md'} mb={2}>{product.product.name}</Heading>
-        <Text fontWeight={'bold'} fontSize={'xl'} color={""} mb={4}>{product.product.price}</Text>
+        <Text fontWeight={'bold'} fontSize={'xl'} color={textColor} mb={4}>{product.product.price}</Text>
         <HStack spacing={2}>
-          <IconButton icon={<EditIcon/>} colorScheme='blue' />
-          <IconButton icon={<DeleteIcon/>}  colorScheme='red' />
+          <IconButton icon={<EditIcon/>} onClick={onOpen} colorScheme='blue' />
+          <IconButton icon={<DeleteIcon/>} onClick={() => handleDeleteProduct(product.product._id)}  colorScheme='red' />
         </HStack>
       </Box>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Update Product</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <VStack>
+              <Input placeholder='Product Name' name='name' value={updatedProduct.product.name}/>
+              <Input placeholder='Product Price' name='price' value={updatedProduct.product.price}/>
+              <Input placeholder='Product Image' name='image' value={updatedProduct.product.image}/>
+            </VStack>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3} >
+              Update
+            </Button>
+            <Button variant='ghost' onClick={onClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   )
 }
