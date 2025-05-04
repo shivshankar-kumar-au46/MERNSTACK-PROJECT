806a2077d6ea2b7cc61a1e07d23c9a535c769378
shiv <kumarshiv417@gmail.com>
2025-05-04 21:19:28 +0530

    handle for deployment

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,27 @@ import express from 'express';
 import { connectDB } from './config/db.js';
 import productRoutes from './routes/product.route.js';
 import dotenv from 'dotenv';
+import path from 'path';
 
 dotenv.config();
 
 const app = express();
+
+const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); //allow us to accept JSON data in the req.body
 
 app.use("/api/products", productRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+    app.get("*",(req,res) => {
+        res.sendFile(path.resolve(__dirname, "frontend","dist","index.html"));
+    })
+}
 
 //OrqJaxsDjT10xZAA
 
